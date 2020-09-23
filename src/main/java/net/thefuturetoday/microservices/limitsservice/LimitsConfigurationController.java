13bf416af1ec9d90dad19490074107689e8133b3
Peter Richards <peter.richards@thefuturetoday.net>
2020-09-23 19:08:21 +0100

    Get limits from properties file

--- src/main/java/net/thefuturetoday/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/net/thefuturetoday/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,12 +1,20 @@
 package net.thefuturetoday.microservices.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
+    private final Configuration configuration;
+
+    public LimitsConfigurationController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfiguration() {
-        return new LimitConfiguration(1000, 1);
+        //getting values from the properties file
+        return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
     }
 }
