3c5f63af8e9a26a079549f0d9e03d3065a2f5294
Peter Richards <peter.richards@thefuturetoday.net>
2020-09-25 21:52:23 +0100

    Enabled Actuator

--- src/main/java/net/thefuturetoday/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/net/thefuturetoday/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,6 +1,5 @@
 package net.thefuturetoday.microservices.limitsservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
