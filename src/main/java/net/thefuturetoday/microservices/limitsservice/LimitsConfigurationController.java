18e21980d5386d1d4cdfd7cfb5f7f2db39e8dc56
Peter Richards <peter.richards@thefuturetoday.net>
2020-09-26 07:46:31 +0100

    Added fault tolerance

--- src/main/java/net/thefuturetoday/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/net/thefuturetoday/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package net.thefuturetoday.microservices.limitsservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,20 @@ public class LimitsConfigurationController {
     public LimitConfiguration retrieveLimitsFromConfiguration() {
         //getting values from the properties file
         return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+    }
+
+    @GetMapping("/fault-tolerance-example")
+    //configuring a fallback method
+    @HystrixCommand(fallbackMethod="fallbackRetrieveConfigurations")
+    public LimitConfiguration retrieveConfigurations()
+    {
+        throw new RuntimeException("Not Available");
+    }
+
+    //defining the fallback method
+    public LimitConfiguration fallbackRetrieveConfigurations()
+    {
+        //returning the default configuration
+        return new LimitConfiguration(999, 9);
     }
 }
